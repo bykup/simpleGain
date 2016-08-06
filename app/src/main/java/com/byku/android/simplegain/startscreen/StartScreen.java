d541a583b0e08646c2f7f588bbcdb952d4400bb1
byku - Przemysław Łybyk <prz.lyb@gmail.com>
2016-08-06 18:18:24 +0200

    Initial commit

---
+++ app/src/main/java/com/byku/android/simplegain/startscreen/StartScreen.java
@@ -0,0 +1,14 @@
+package com.byku.android.simplegain.startscreen;
+
+import android.os.Bundle;
+
+import com.byku.android.simplegain.BaseActivity;
+
+public class StartScreen extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
